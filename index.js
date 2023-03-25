c28b03e8b1d4a1ca3a99804d3193ade63bf366f7
DESKTOP-332GF4V\kkary <nambawon1@naver.com>
2023-03-25 19:31:07 +0900

    Test

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/mongo');
-  console.log('Connect!');
+  console.log('Connect!!!');
 }
