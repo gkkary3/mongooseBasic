c4a2138ba718ce59e4b978ff052e6a5a5a28147a
DESKTOP-332GF4V\kkary <nambawon1@naver.com>
2023-03-25 19:23:42 +0900

    mongoose Basic

---
+++ index.js
@@ -0,0 +1,9 @@
+// getting-started.js
+const mongoose = require('mongoose');
+
+main().catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/mongo');
+  console.log('Connect!');
+}
